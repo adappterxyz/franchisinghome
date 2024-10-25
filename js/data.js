b2b400ecda44638a1c9d03dbc4bf99d0c7a1ff39
AdappterXYZ <kwang@adappter.xyz>
2024-10-26 00:32:34 +0800

    images

--- js/data.js
+++ js/data.js
@@ -1,73 +1,73 @@
 var table = [
-    "https://www.mcdonalds.com/us/en-us.html|https://logos-world.net/wp-content/uploads/2020/04/McDonalds-Logo.png", 
+    "https://www.mcdonalds.com/us/en-us.html|./img/favicon.png", 
     "The Story of McDonald's: From a Single Restaurant to a Global Franchise", 
     "05/Sep", 1, 1,
 
-    "https://www.subway.com/en-US|https://logos-world.net/wp-content/uploads/2020/05/Subway-Logo.png", 
+    "https://www.subway.com/en-US|./img/subway.png", 
     "How Subway Revolutionized the Fast-Food Industry with Customization", 
     "05/Sep", 1, 2,
 
-    "https://www.kfc.com|https://logos-world.net/wp-content/uploads/2020/04/KFC-Logo.png", 
+    "https://www.kfc.com|./img/favicon.png", 
     "KFC: The Colonel’s Recipe for Global Success", 
     "05/Sep", 1, 3,
 
-    "https://www.starbucks.com|https://logos-world.net/wp-content/uploads/2020/04/Starbucks-Logo.png", 
+    "https://www.starbucks.com|./img/starbucks.png", 
     "Starbucks: How a Coffeehouse Became a Lifestyle", 
     "05/Sep", 1, 4,
 
-    "https://www.dominos.com|https://logos-world.net/wp-content/uploads/2020/04/Dominos-Pizza-Logo.png", 
+    "https://www.dominos.com|./img/domino.jpg", 
     "Domino's Pizza: Innovating Delivery and Customer Experience", 
     "05/Sep", 1, 5,
 
-    "https://www.marriott.com|https://logos-world.net/wp-content/uploads/2020/04/Marriott-Logo.png", 
+    "https://www.marriott.com|./img/marriot.png", 
     "Marriott Hotels: Building a Global Hospitality Empire", 
     "05/Sep", 1, 6,
 
-    "https://www.hilton.com|https://logos-world.net/wp-content/uploads/2020/04/Hilton-Logo.png", 
+    "https://www.hilton.com|./img/hilton.jpg", 
     "Hilton: The Story Behind One of the Largest Hotel Chains in the World", 
     "05/Sep", 1, 7,
 
-    "https://www.pizzahut.com|https://logos-world.net/wp-content/uploads/2020/04/Pizza-Hut-Logo.png", 
+    "https://www.pizzahut.com|./img/pizza.png", 
     "Pizza Hut: From Kansas to Over 18,000 Locations Worldwide", 
     "05/Sep", 1, 8,
 
-    "https://www.hardrockcafe.com|https://logos-world.net/wp-content/uploads/2020/04/Hard-Rock-Cafe-Logo.png", 
+    "https://www.hardrockcafe.com|./img/hardrock.png", 
     "Hard Rock Cafe: Merging Rock Music and Dining into a Global Franchise", 
     "05/Sep", 1, 9,
 
-    "https://www.burgerking.com|https://logos-world.net/wp-content/uploads/2020/04/Burger-King-Logo.png", 
+    "https://www.burgerking.com|./img/favicon.png", 
     "Burger King: The Battle for the Burger Crown", 
     "05/Sep", 2, 1,
 
-    "https://www.dunkindonuts.com|https://logos-world.net/wp-content/uploads/2020/04/Dunkin-Logo.png", 
+    "https://www.dunkindonuts.com|./img/favicon.png", 
     "Dunkin': From Donuts to Coffee, a Brand Evolution", 
     "05/Sep", 2, 2,
 
-    "https://www.7-eleven.com|https://logos-world.net/wp-content/uploads/2020/04/7-Eleven-Logo.png", 
+    "https://www.7-eleven.com|./img/favicon.png", 
     "7-Eleven: The World’s Largest Convenience Store Franchise", 
     "05/Sep", 2, 3,
 
-    "https://www.hertz.com|https://logos-world.net/wp-content/uploads/2020/04/Hertz-Logo.png", 
+    "https://www.hertz.com|./img/favicon.png", 
     "Hertz: Pioneering the Car Rental Business", 
     "05/Sep", 2, 4,
 
-    "https://www.ihop.com|https://logos-world.net/wp-content/uploads/2020/04/IHOP-Logo.png", 
+    "https://www.ihop.com|./img/favicon.png", 
     "IHOP: How a Pancake House Became a Breakfast Powerhouse", 
     "05/Sep", 2, 5,
 
-    "https://www.chick-fil-a.com|https://logos-world.net/wp-content/uploads/2020/04/Chick-fil-A-Logo.png", 
+    "https://www.chick-fil-a.com|./img/favicon.png", 
     "Chick-fil-A: The Chicken Sandwich That Conquered Fast Food", 
     "05/Sep", 2, 6,
 
-    "https://www.carljr.com|https://logos-world.net/wp-content/uploads/2020/04/Carls-Jr-Logo.png", 
+    "https://www.carljr.com|./img/favicon.png", 
     "Carl's Jr.: A Journey from a Hot Dog Stand to a Fast Food Giant", 
     "05/Sep", 2, 7,
 
-    "https://www.tacobell.com|https://logos-world.net/wp-content/uploads/2020/04/Taco-Bell-Logo.png", 
+    "https://www.tacobell.com|./img/favicon.png", 
     "Taco Bell: Bringing Mexican-Inspired Fast Food to the World", 
     "05/Sep", 2, 8,
 
-    "https://www.wendys.com|https://logos-world.net/wp-content/uploads/2020/04/Wendys-Logo.png", 
+    "https://www.wendys.com|./img/favicon.png", 
     "Wendy’s: The Fast Food Chain Built on Freshness", 
     "05/Sep", 2, 9
 ];
